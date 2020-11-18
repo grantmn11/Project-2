f1b917ab689105cdb84ea677d3449c8b48303f86
grantmn11 <grantferreira11@gmail.com>
2020-11-17 17:32:51 -0800

    mongoDB connect

---
+++ config/database.js
@@ -0,0 +1,17 @@
+// require modules
+const mongoose = require('mongoose')
+
+// set up shortcut variable (db)
+const db = mongoose.connection;
+
+// connect to datavase server
+mongoose.connect('mongodb://localhost/users', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+// listener
+db.on('connected', function(){
+    console.log(`Connected to MongoDB on ${db.host}: ${db.port}`)
+})
